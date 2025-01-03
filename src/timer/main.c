9f0726119b5ceab1624c1b0a74cb144ea1f810ac
Taha Brida <mohamed-taha.brida@ecole.ensicaen.fr>
2025-01-03 18:30:01 +0100

    Add error handling function in timer.c

--- src/timer/main.c
+++ src/timer/main.c
@@ -1,4 +1,11 @@
-// main.c
+#include <stdio.h>
+#include "timer.h"
+
 int main() {
+    printf("Testing error management\n");
+
+    // Simulate an error condition and call the function
+    manage_error_and_exit();
+
     return 0;
 }
--- src/timer/timer.c
+++ src/timer/timer.c
@@ -1,5 +1,12 @@
-// timer.c
+#include <stdlib.h>
+#include <stdio.h>
 #include "timer.h"
-void manage_error_and_exit() {}
+
+void manage_error_and_exit() {
+    perror("Critical error occurred!");
+    exit(EXIT_FAILURE);
+}
+
 void handle_alarm(int sig) {}
+
 void set_timer(int target_pid, int interval) {}
